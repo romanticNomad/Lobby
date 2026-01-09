e8360c5c04cad8b341feea24a97638699e191f30
romanticNomad <satvolume@gmail.com>
2026-01-09 09:27:57 +0000

    chore: remove Rust target directories from version control

--- lobby/target/debug/deps/auth-334f5722cd859c1e.d
+++
@@ -1,5 +0,0 @@
-/home/hope/matrix/vault/Lobby/lobby/target/debug/deps/auth-334f5722cd859c1e.d: auth/src/main.rs
-
-/home/hope/matrix/vault/Lobby/lobby/target/debug/deps/libauth-334f5722cd859c1e.rmeta: auth/src/main.rs
-
-auth/src/main.rs:
--- lobby/target/debug/deps/memory-4c3f2d72b93a00b9.d
+++
@@ -1,5 +0,0 @@
-/home/hope/matrix/vault/Lobby/lobby/target/debug/deps/memory-4c3f2d72b93a00b9.d: memory/src/main.rs
-
-/home/hope/matrix/vault/Lobby/lobby/target/debug/deps/libmemory-4c3f2d72b93a00b9.rmeta: memory/src/main.rs
-
-memory/src/main.rs:
