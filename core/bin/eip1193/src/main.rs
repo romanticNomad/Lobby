13ed4a489b873560544f01746de3aad39e76308e
romanticNomad <satvolume@gmail.com>
2026-01-13 11:51:24 +0000

    POST endpoint setup

--- core/bin/eip1193/src/main.rs
+++ core/bin/eip1193/src/main.rs
@@ -1,6 +1,17 @@
+mod handler;
 #[allow(dead_code)]
 mod protocol;
 
-fn main() {
-    println!("Hello, world!");
+use axum::{Router, routing::post};
+use tokio::net::TcpListener;
+
+#[tokio::main]
+async fn main() {
+    let app = Router::new().route("/", post(handler::rpc_handler));
+
+    let listner = TcpListener::bind("0.0.0.0:8454")
+        .await
+        .expect("address binding failed!");
+
+    axum::serve(listner, app).await.expect("server failed");
 }
