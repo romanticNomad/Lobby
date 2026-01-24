87be1662fa95087367b4d180f0ab716eebd5d2ba
romanticNomad <satvolume@gmail.com>
2026-01-24 10:59:51 +0000

    NonceManager implimented for NonceChannel

--- core/crate/actors/src/nonce.rs
+++ core/crate/actors/src/nonce.rs
@@ -1,50 +1,98 @@
-// use alloy_primitives::Address;
-// use async_trait::async_trait;
-// use kernel::{
-//     traits::NonceManager,
-//     types::{ChainId, ExecutionError, ExecutionId, TxNonce},
-// };
-// use tokio::sync::{mpsc, oneshot};
+use alloy_primitives::Address;
+use async_trait::async_trait;
+use kernel::{
+    traits::NonceManager,
+    types::{ChainId, ExecutionError, ExecutionId, TxNonce},
+};
+use tokio::sync::{mpsc, oneshot};
 
-// // =========================================================
-// // Commands to send over the channel.
+// =========================================================
+// Commands to send over the channel.
 
-// pub enum NonceCommand {
-//     Reserve {
-//         chain_id: ChainId,
-//         from: Address,
-//         id: ExecutionId,
-//         reply: oneshot::Sender<Result<TxNonce, ExecutionError>>,
-//     },
-//     Resolve {
-//         chain_id: ChainId,
-//         from: Address,
-//         id: ExecutionId,
-//         outcome: bool,
-//         reply: oneshot::Sender<Result<(), ExecutionError>>,
-//     },
-// }
+pub enum NonceCommand {
+    Reserve {
+        chain_id: ChainId,
+        from: Address,
+        id: ExecutionId,
+        reply: oneshot::Sender<Result<TxNonce, ExecutionError>>,
+    },
+    Resolve {
+        chain_id: ChainId,
+        from: Address,
+        id: ExecutionId,
+        outcome: bool,
+        reply: oneshot::Sender<Result<(), ExecutionError>>,
+    },
+}
 
-// // =========================================================
-// // Entry point for Lobby into nonce actor.
+// =========================================================
+// Entry point for Lobby into nonce actor.
 
-// #[derive(Clone)]
-// pub struct NonceChannel {
-//     tx: mpsc::Sender<NonceCommand>,
-// }
+#[derive(Clone)]
+pub struct NonceChannel {
+    tx: mpsc::Sender<NonceCommand>,
+}
 
-// impl NonceChannel {
-//     pub fn new(tx: mpsc::Sender<NonceCommand>) -> Self {
-//         Self { tx }
-//     }
-// }
+impl NonceChannel {
+    pub fn new(tx: mpsc::Sender<NonceCommand>) -> Self {
+        Self { tx }
+    }
+}
 
-// // =========================================================
-// // implimentation of NonceManager for NonceChannel.
+// =========================================================
+// implimentation of NonceManager for NonceChannel.
 
-// #[async_trait]
-// impl NonceManager for NonceChannel {
-//     async fn reserve(nonce_id: TxNonce, from: Address, id: ExecutionId) -> Result<TxNonce, ExecutionError> {
+#[async_trait]
+impl NonceManager for NonceChannel {
+    async fn reserve(
+        &self,
+        chain_id: ChainId,
+        from: Address,
+        id: ExecutionId,
+    ) -> Result<TxNonce, ExecutionError> {
+        let (reply_tx, reply_rx) = oneshot::channel();
 
-//     }
-// }
+        let cmd = NonceCommand::Reserve {
+            chain_id,
+            from,
+            id,
+            reply: reply_tx,
+        };
+
+        self.tx
+            .send(cmd)
+            .await
+            .map_err(|_| ExecutionError::Internal("NonceActor not available".to_string()))?;
+
+        reply_rx
+            .await
+            .map_err(|_| ExecutionError::Internal("NonceActor response corrupted".to_string()))?
+    }
+
+    async fn resolve(
+        &self,
+        chain_id: ChainId,
+        from: Address,
+        id: ExecutionId,
+        outcome: bool,
+    ) -> Result<(), ExecutionError> {
+        let (reply_tx, reply_rx) = oneshot::channel();
+
+        let cmd = NonceCommand::Resolve {
+            chain_id,
+            from,
+            id,
+            outcome,
+            reply: reply_tx,
+        };
+
+        self.tx
+            .send(cmd)
+            .await
+            .map_err(|_| ExecutionError::Internal("NonceActor unavailable".into()))?;
+
+        reply_rx
+            .await
+            .map_err(|_| ExecutionError::Internal("NonceActor dropped response".into()))?
+    }
+}
