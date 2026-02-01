c12b997a5cd6f9402c2e4a63718895e3d711301b
romanticNomad <satvolume@gmail.com>
2026-02-01 12:20:41 +0000

    evm compatible keccak-256 test

--- core/crate/props/src/evm_signer.rs
+++ core/crate/props/src/evm_signer.rs
@@ -67,3 +67,23 @@ pub fn sign_eip1559_transaction(
 }
 
 // ============================================================
+
+#[cfg(test)]
+mod test {
+    use alloy_primitives::Keccak256;
+    use hex_literal::hex;
+    
+    #[test]
+    fn hasher_check_evm_keccak_256() {
+        let mut hash = Keccak256::new();
+        hash.update([]);
+        let output = hash.finalize();
+
+        let keccak_expected = hex!("c5d2460186f7233c927e7db2dcc703c0e500b653ca82273b7bfad8045d85a470");
+        assert_eq!(
+            output.as_slice(),
+            keccak_expected,
+            "Hasher is not ethereum Keccak-256"
+        );
+    }
+}
