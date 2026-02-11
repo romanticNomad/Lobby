3b154a124ca5419b7731319ac75d866d6385addd
romanticNomad <satvolume@gmail.com>
2026-02-11 17:27:21 +0530

    relay host engine ambiguity ?

--- core/crate/netio/src/relayhost/engine.rs
+++ core/crate/netio/src/relayhost/engine.rs
@@ -1,1 +1,60 @@
+// use kernel::types::{ClientConfig, Eip1559Transaction, ExecutionId, RelayHostError};
+// use sqlx::PgPool;
+// use tokio::sync::mpsc;
+// use tracing::info;
 
+// use crate::relayhost::handle::{RelayHostCommand, RelayHostHandle};
+
+// // ============================================================
+// // RelatHostEngine and spawn implimentation
+
+// pub struct RelayHostEngine {
+//     db: PgPool,
+//     rx: mpsc::Receiver<RelayHostCommand>,
+// }
+
+// impl RelayHostEngine {
+//     pub fn spawn(db: PgPool) -> RelayHostHandle {
+//         let (tx, rx) = mpsc::channel(1024);
+//         let relayhost_actor = RelayHostEngine { db, rx };
+
+//         tokio::spawn(async move {
+//             relayhost_actor.run().await;
+//         });
+
+//         RelayHostHandle::new(tx)
+//     }
+// }
+
+// // ============================================================
+// // RelayHostEngine functioning
+
+// impl RelayHostEngine {
+//     pub async fn run(mut self) {
+//         info!("RelayHostEngine started");
+
+//         while let Some(msg) = self.rx.recv().await {
+//             match msg {
+//                 RelayHostCommand::SubmitTransaction {
+//                     execution_id,
+//                     txn,
+//                     client_config,
+//                     reply_tx,
+//                 } => {
+//                     let result = self
+//                         .handle_submit_transaction(execution_id, txn, client_config).await;
+//                     let _ = reply_tx.send(result);
+//                 }
+//             }
+//         }
+//     }
+
+//     async fn handle_submit_transaction(
+//         &self,
+//         execution_id: ExecutionId,
+//         txn: Eip1559Transaction,
+//         client_config: ClientConfig
+//     ) -> Result<(), RelayHostError> {
+
+//     }
+// }
