8ad5962cbc1e7f7c359839d558a576dcf7a453ec
romanticNomad <satvolume@gmail.com>
2026-02-11 23:30:21 +0530

    transaction lint draft 1

--- core/crate/utils/src/eip1159_linter.rs
+++ core/crate/utils/src/eip1159_linter.rs
@@ -1,1 +1,70 @@
+// this module only consists of linting functions that do not require
+// database lookup .i.e, it only check business logic for eip1159
+// transaction fields.
 
+use alloy::primitives::U256;
+use kernel::types::{Eip1559Transaction, RelayHostError};
+
+// ============================================================
+
+pub fn transaction_lint(txn: &Eip1559Transaction) -> Result<(), RelayHostError> {
+    // ============================================================
+    // gas limit check
+
+    if txn.gas_limit == U256::ZERO {
+        return Err(RelayHostError::ValidationFailed(
+            "gas limit cannot be zero".to_string(),
+        ));
+    };
+
+    // ============================================================
+    // max gas limit (30M gas)
+
+    const MAX_GAS_LIMIT: u64 = 30_000_000;
+    if txn.gas_limit > U256::from(MAX_GAS_LIMIT) {
+        return Err(RelayHostError::ValidationFailed(format!(
+            "fee exeeds max gas limit: {}",
+            MAX_GAS_LIMIT
+        )));
+    };
+
+    // ============================================================
+    // fee validation
+
+    if txn.max_fee_per_gas == U256::ZERO {
+        return Err(RelayHostError::ValidationFailed(
+            "max_fee_per_gas cannot be zero".to_string(),
+        ));
+    };
+
+    if txn.max_priority_fee_per_gas >= txn.max_fee_per_gas {
+        return Err(RelayHostError::ValidationFailed(
+            "max_priority_fee_per_gas cannot exceed max_fee_per_gas".to_string(),
+        ));
+    };
+
+    // ============================================================
+    // chain_id validation
+
+    const SUPPORTED_CHAINS: &[u64] = &[
+        1,        // Ethereum Mainnet
+        5,        // Goerli
+        11155111, // Sepolia
+        137,      // Polygon
+        42161,    // Arbitrum One
+    ];
+
+    if !SUPPORTED_CHAINS.contains(
+        &(txn.chain_id.0)
+            .try_into()
+            .map_err(|_| RelayHostError::ValidationFailed("invalid chain_id format".to_string()))?,
+    ) {
+        return Err(RelayHostError::ValidationFailed(
+            "unsupported chain_id".to_string(),
+        ));
+    };
+
+    // ============================================================
+
+    Ok(())
+}
