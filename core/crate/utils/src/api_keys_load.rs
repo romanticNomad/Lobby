49e7701cc7f8600a56bbc645d0c54fa665d1a834
romanticNomad <satvolume@gmail.com>
2026-02-14 20:24:01 +0530

    function load_api_key_from_env drafted

--- core/crate/utils/src/api_keys_load.rs
+++ core/crate/utils/src/api_keys_load.rs
@@ -1,34 +1,65 @@
-// use std::env;
+use std::{env, str::FromStr};
 
-// use alloy::primitives::Address;
-// use dashmap::DashMap;
-// use kernel::types::{ApiKey, ClientConfig};
-// use uuid::Uuid;
+use alloy::primitives::Address;
+use dashmap::DashMap;
+use kernel::types::{ApiKey, ClientConfig};
+use uuid::Uuid;
 
-// // ============================================================
-// // function for loading api keys from the env (for dev testing only)
+// ============================================================
 
-// pub fn load_api_key_from_env() -> Result<DashMap<ApiKey, ClientConfig>, Box<dyn std::error::Error>> {
-//     let api_keys = DashMap::new();
+#[derive(Debug)]
+pub enum EnvApiKeyError {
+    InvalidClientId(String),
+    InvalidFromAddress(String),
+    InvalidApiKey(String),
+    ApiKeyUnavailable,
+}
 
-//     // Format: LOBBY_API_KEY_<N>=<api_key>:<client_id>:<from_address>
-//     for (key, value) in env::vars() {
-//         if let Some(suffix) = key.strip_prefix("LOBBY_API_KEY_".to_string()) {
-//             let parts:Vec<&str> = value.split(":").collect();
-//             if parts.len() != 3 {
-//                 return Err(format!(
-//                     "Invalid API key format for {}: expected <api_key>:<client_id>:<from_address>",
-//                     key
-//                 )
-//                 .into())
-//             }
+// ============================================================
+// function for loading api keys from the env (for dev testing only)
 
-//             let apki_key = parts[0].to_string();
-//             let client_id = Uuid::parse_str(parts[1])
-//                 .map_err(|_| format!("invalid client id: {}", key))?;
-//             let from_address = Address::from_str(parts[2].to_string());
-//         }
-//     }
-// }
+pub fn load_api_key_from_env() -> Result<DashMap<ApiKey, ClientConfig>, EnvApiKeyError> {
+    let api_keys = DashMap::new();
 
-// // ============================================================
+    // Format: LOBBY_API_KEY_<N>=<api_key>:<client_id>:<from_address>
+    for (key, value) in env::vars() {
+        if let Some(suffix) = key.strip_prefix("LOBBY_API_KEY_") {
+            let parts:Vec<&str> = value.split(":").collect();
+            if parts.len() != 3 {
+                return Err(EnvApiKeyError::InvalidApiKey(format!(
+                    "Invalid API key format for {}: expected <api_key>:<client_id>:<from_address>",
+                    key
+                ))
+                .into())
+            }
+
+            let api_key = parts[0].to_string();
+            let client_id = Uuid::parse_str(parts[1])
+                .map_err(|_| EnvApiKeyError::InvalidClientId(format!("invalid client id: {}", key)))?;
+            let from_address = Address::from_str(parts[2])
+                .map_err(|_| EnvApiKeyError::InvalidFromAddress(format!("invalid from_address {}", key)))?;
+
+            let client_config = ClientConfig {
+                client_id,
+                from_address
+            };
+
+            api_keys.insert(api_key, client_config);
+
+            tracing::debug!(
+                key = suffix,
+                client_id = %client_id,
+                from_address = ?from_address,
+                "loaded api key"                
+            );
+        }
+    }
+
+    if api_keys.is_empty() {
+        return Err(EnvApiKeyError::ApiKeyUnavailable);
+    }
+
+    Ok(api_keys)
+}
+
+// ============================================================
