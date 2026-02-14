5cdda7d00fad790213f7597591ed9304b9ab66e8
romanticNomad <satvolume@gmail.com>
2026-02-14 20:02:43 +0530

    checksum removed

--- core/crate/utils/src/api_keys_load.rs
+++ core/crate/utils/src/api_keys_load.rs
@@ -1,37 +1,34 @@
-use std::env;
+// use std::env;
 
-use alloy::primitives::Address;
-use dashmap::DashMap;
-use kernel::types::{ApiKey, ClientConfig};
-use uuid::Uuid;
+// use alloy::primitives::Address;
+// use dashmap::DashMap;
+// use kernel::types::{ApiKey, ClientConfig};
+// use uuid::Uuid;
 
-// ============================================================
-// function for loading api keys from the env (for dev testing only)
+// // ============================================================
+// // function for loading api keys from the env (for dev testing only)
 
-pub fn load_api_key_from_env() -> Result<DashMap<ApiKey, ClientConfig>, Box<dyn std::error::Error>>
-{
-    let api_keys = DashMap::new();
+// pub fn load_api_key_from_env() -> Result<DashMap<ApiKey, ClientConfig>, Box<dyn std::error::Error>> {
+//     let api_keys = DashMap::new();
 
-    // Format: LOBBY_API_KEY_<N>=<api_key>:<client_id>:<from_address>
-    for (key, value) in env::vars() {
-        if let Some(suffix) = key.strip_prefix("LOBBY_API_KEY_".to_string()) {
-            let parts: Vec<&str> = value.split(":").collect();
-            if parts.len() != 3 {
-                return Err(format!(
-                    "Invalid API key format for {}: expected <api_key>:<client_id>:<from_address>",
-                    key
-                )
-                .into());
-            }
+//     // Format: LOBBY_API_KEY_<N>=<api_key>:<client_id>:<from_address>
+//     for (key, value) in env::vars() {
+//         if let Some(suffix) = key.strip_prefix("LOBBY_API_KEY_".to_string()) {
+//             let parts:Vec<&str> = value.split(":").collect();
+//             if parts.len() != 3 {
+//                 return Err(format!(
+//                     "Invalid API key format for {}: expected <api_key>:<client_id>:<from_address>",
+//                     key
+//                 )
+//                 .into())
+//             }
 
-            let apki_key = parts[0].to_string();
-            let client_id =
-                Uuid::parse_str(parts[1]).map_err(|_| format!("invalid client id: {}", key))?;
-            let from_address = Address::from(parts[2].to_string());
-        }
-    }
+//             let apki_key = parts[0].to_string();
+//             let client_id = Uuid::parse_str(parts[1])
+//                 .map_err(|_| format!("invalid client id: {}", key))?;
+//             let from_address = Address::from_str(parts[2].to_string());
+//         }
+//     }
+// }
 
-    Ok(())
-}
-
-// ============================================================
+// // ============================================================
--- core/crate/utils/src/eip1159_normalize.rs
+++ core/crate/utils/src/eip1159_normalize.rs
@@ -1,5 +1,3 @@
-use std::str::FromStr;
-
 use alloy::primitives::{Address, U256, bytes::Bytes};
 
 use kernel::types::{ChainId, Eip1193SendTransactionParams, Eip1559Transaction, TxNonce};
@@ -97,12 +95,7 @@ pub fn normalize_eip1193_transaction(
         access_list,
     };
 
-    let addr_checksum = params.from.to_checksum(None);
-    let addr = Address::from_str(&addr_checksum).map_err(|_| {
-        NormalizationError::InvalidField(format!("invalid address: {}", addr_checksum))
-    })?;
-
-    Ok((transaction, addr))
+    Ok((transaction, params.from))
 }
 
 // ============================================================
