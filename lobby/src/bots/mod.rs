1b299da5d038b15e05c0ea41e8ae56d6d43cfb85
romanticNomad <satvolume@gmail.com>
2026-04-18 20:26:08 +0530

    scanner bot deprecated until further updates

--- lobby/src/bots/mod.rs
+++ lobby/src/bots/mod.rs
@@ -1,4 +1,5 @@
-pub mod scanner;
+// scanner_bot module needs development with respect to Load Balancing changes made to the RPC crate.
+// pub mod scanner;
 pub mod sweeper;
 
 // pub use scanner::*;
