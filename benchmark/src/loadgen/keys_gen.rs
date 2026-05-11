5a2ce6c18fdc8d5876c0727354c95b471e0ce666
romanticNomad <satvolume@gmail.com>
2026-05-12 03:02:25 +0530

    fmt

--- benchmark/src/loadgen/keys_gen.rs
+++ benchmark/src/loadgen/keys_gen.rs
@@ -6,7 +6,7 @@ use std::collections::HashMap;
 
 /// Registry to store the dummy `evm-key`(s) and corresponding `api-key`(s)
 pub struct KeyRegistry {
-    /// Hasmap to store the (public, private) keys in a (key, value) pair
+    /// Hasmap to store the `(public, private)` keys in a (key, value) pair
     evm_key: HashMap<Address, [u8; 32]>,
 
     /// ApiStack to store the lobby `Api-Key` generated for a given `from_address`
