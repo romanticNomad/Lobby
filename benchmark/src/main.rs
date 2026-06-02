9da5b287d0cd233681c67b03bb56563e1c913c56
romanticNomad <satvolume@gmail.com>
2026-06-02 23:03:43 +0530

    rexports for loadgen

--- benchmark/src/main.rs
+++ benchmark/src/main.rs
@@ -1,11 +1,11 @@
-use anyhow::Ok;
-
 #[allow(dead_code)]
 mod infra;
-#[allow(dead_code)]
+#[allow(dead_code, unused_imports)]
 mod loadgen;
 mod metrics;
 mod mockrpc;
+
+use anyhow::Ok;
 
 #[tokio::main]
 async fn main() -> Result<(), anyhow::Error> {
