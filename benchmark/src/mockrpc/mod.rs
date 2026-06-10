a0450674f2f9fbe6421f90e666aacd9fbb2f730d
romanticNomad <satvolume@gmail.com>
2026-06-10 14:02:24 +0530

    updated contract to accomodate Null value on receipt not found

--- benchmark/src/mockrpc/mod.rs
+++ benchmark/src/mockrpc/mod.rs
@@ -1,10 +1,9 @@
 mod router;
 mod state;
 
-use crate::mockrpc::state::{NonceUpdateOutcome, StaticReceipt};
-use alloy::primitives::TxHash;
-use alloy::rpc::types::TransactionReceipt;
-use async_trait::async_trait;
+use crate::mockrpc::state::NonceUpdateOutcome;
+use alloy::primitives::{Address, TxHash};
+use serde_json::value::RawValue;
 use std::sync::Arc;
 // ============================================================
 // state contract
@@ -14,20 +13,20 @@ pub trait MockRpcState: Send + Sync {
     /// Validates and updates the stored nonce [`AtomicU64`] in the `NonceState`,
     ///
     /// ## Retruns
-    /// * `StateUpdateOutcome::NonceAdvanced(u64)`, when updated successfully
-    /// * `StateUpdateOutcome::NonceTooLow` in case of `rlp_nonce` < `existing_nonce`
+    /// * `NonceUpdateOutcome::NonceAdvanced(u64)`, when updated successfully
+    /// * `NonceUpdateOutcome::NonceTooLow` in case of `rlp_nonce` < `existing_nonce`
     ///
     /// ## Note:
     /// for a controled system like benchmark, it is expected that `rlp_nonce` will not be greated than
     /// `registered nonce`.
-    fn update_nonce(&self, address: String, nonce_rlp: u64) -> NonceUpdateOutcome;
+    fn update_nonce(&self, address: Address, nonce_rlp: u64) -> NonceUpdateOutcome;
 
     /// To reduce client overhead in the benchmarking process, the transction receipts are pre-generated,
     ///
     /// ## Returns
-    /// * `TransactionReceipt`, if the receipt is found
-    /// * panics if the `tx_hash` is not already registere.
-    fn fetch_receipt(&self, tx_hash: TxHash) -> TransactionReceipt;
+    /// * `Some(Arc<Box<RawValue>>)`: return raw_value, with zero heap cloning.
+    /// * `None`: when no receipt is yet registered.
+    fn fetch_receipt(&self, tx_hash: &TxHash) -> Option<Arc<Box<RawValue>>>;
 }
 
 // ============================================================
